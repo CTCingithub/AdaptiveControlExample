6110dec6c6f3dc2023dc04a0a899727a150dc887
CTC <2801320287@qq.com>
2024-09-19 13:59:33 +0800

    Parameters from Jiang's Simulation

--- 2DoFSerialManipulator/DynRegVec.m
+++ 2DoFSerialManipulator/DynRegVec.m
@@ -12,9 +12,9 @@ function DynRegVec = DynRegVec(theta_1, theta_2, v_1, v_2, a_1, a_2)
         0;
         a_1;
         0;
-        g.*cos(theta_1);
+        9.8*cos(theta_1);
         0;
-        -g.*sin(theta_1);
+        -9.8*sin(theta_1);
         0;
         0;
         0;
@@ -32,13 +32,13 @@ function DynRegVec = DynRegVec(theta_1, theta_2, v_1, v_2, a_1, a_2)
         0;
         a_1 + a_2;
         a_1 + a_2;
-        2*a_1.*l1.*cos(theta_2) + a_2.*l1.*cos(theta_2) + g.*cos(theta_1 + theta_2) - 2*l1.*v_1.*v_2.*sin(theta_2) - l1.*v_2.^2.*sin(theta_2);
-        a_1.*l1.*cos(theta_2) + g.*cos(theta_1 + theta_2) + l1.*v_1.^2.*sin(theta_2);
-        -2*a_1.*l1.*sin(theta_2) - a_2.*l1.*sin(theta_2) - g.*sin(theta_1 + theta_2) - 2*l1.*v_1.*v_2.*cos(theta_2) - l1.*v_2.^2.*cos(theta_2);
-        -a_1.*l1.*sin(theta_2) - g.*sin(theta_1 + theta_2) + l1.*v_1.^2.*cos(theta_2);
+        3.4*a_1.*cos(theta_2) + 1.7*a_2.*cos(theta_2) - 3.4*v_1.*v_2.*sin(theta_2) - 1.7*v_2.^2.*sin(theta_2) + 9.8*cos(theta_1 + theta_2);
+        1.7*a_1.*cos(theta_2) + 1.7*v_1.^2.*sin(theta_2) + 9.8*cos(theta_1 + theta_2);
+        -3.4*a_1.*sin(theta_2) - 1.7*a_2.*sin(theta_2) - 3.4*v_1.*v_2.*cos(theta_2) - 1.7*v_2.^2.*cos(theta_2) - 9.8*sin(theta_1 + theta_2);
+        -1.7*a_1.*sin(theta_2) + 1.7*v_1.^2.*cos(theta_2) - 9.8*sin(theta_1 + theta_2);
         0;
         0;
-        l1.*(a_1.*l1 + g.*cos(theta_1));
+        2.89*a_1 + 16.66*cos(theta_1);
         0
         ];
 end
